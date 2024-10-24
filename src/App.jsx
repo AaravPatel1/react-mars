2d183371b12df3dfaa55bdc0ca11ee7cfc47b53d
aarav patel <aaravpatel@aaravs-MacBook-Pro.local>
2024-10-24 14:23:59 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import React from 'react';
+import MarsWeather from '../../../combined_lab_project/react_mars_weather/MarsWeather';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <MarsWeather />
+      </header>
+    </div>
+  );
+}
+
+export default App;
